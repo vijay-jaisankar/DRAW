540af760c21fd97d6f90462eaa5b6b96ed970dbd
Vijay Jaisankar <56185979+vijay-jaisankar@users.noreply.github.com>
2020-01-26 12:23:38 +0530

    Uploading files

---
+++ canvas.js
@@ -0,0 +1,50 @@
+window.addEventListener("load",()=> {
+	const canvas = document.querySelector("#canvas");
+	const ctx = canvas.getContext("2d");
+
+	//Resizing
+	
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	
+	//Variables
+	
+	let painting = false;
+
+	function startPosition(e)
+	{
+		painting = true;
+		draw(e);
+	}
+	function finishedPosition()
+	{
+		painting = false;
+		ctx.beginPath();
+	}
+	function draw(e)
+	{
+		if(!painting)
+		{
+			return;
+		}
+		ctx.lineWidth = 10;
+		ctx.lineCap = "round";
+		//ctx.strokeStyle = "red";
+		ctx.lineTo(e.clientX,e.clientY);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(e.clientX,e.clientY);
+	}
+
+
+	
+	//EventListeners
+	
+	canvas.addEventListener("mousedoen",startPosition);
+	canvas.addEventListener("mouseup",finishedPosition);
+	canvas.addEventListener("mousemove",draw);
+	
+
+	
+
+});
